f47f456c0491b6c8ad2877337e64872b96f51302
falc00 <falcao.goncalo00@gmail.com>
2021-12-04 18:09:25 +0000

    libs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pandas import read_csv
 from pandas.plotting import register_matplotlib_converters
 from matplotlib.pyplot import figure, savefig, show
-from ds_charts import bar_chart
+from libs.ds_charts import bar_chart
 
 register_matplotlib_converters()
 
@@ -9,3 +9,9 @@ filename = 'dataset_1/NYC_collisions_tabular.csv'
 data = read_csv(filename, na_values='na')
 
 print(data.shape)
+
+figure(figsize=(4,2))
+values = {'nr records': data.shape[0], 'nr variables': data.shape[1]}
+bar_chart(list(values.keys()), list(values.values()), title='Nr of records vs nr variables')
+savefig('images/records_variables.png')
+show()
